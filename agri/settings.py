c8940dcae096e262e1011f5635781923113f5feb
itsMisbah <misbah77777@gmail.com>
2026-03-28 02:14:36 +0500

    handle env

--- agri/settings.py
+++ agri/settings.py
@@ -7,14 +7,11 @@ load_dotenv()
 
 env = environ.Env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env( env_file=BASE_DIR / '.env' )
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+if os.environ.get("RAILWAY_ENV") != "production":
+    environ.Env.read_env( env_file=BASE_DIR / '.env' )
+    
 SECRET_KEY = env("SECRET_KEY")
 
 
