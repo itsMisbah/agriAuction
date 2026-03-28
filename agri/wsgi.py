e9082cbcbf50f140f2cf1ee1f9898f856385af75
itsMisbah <misbah77777@gmail.com>
2026-03-28 05:09:44 +0500

    remove .toml file

--- agri/wsgi.py
+++ agri/wsgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agri.settings')
+
+application = get_wsgi_application()
+
+# Auto migrate on startup
+try:
+    call_command('migrate', '--noinput')
+except Exception as e:
+    print(f"Migration error: {e}")
 
 application = get_wsgi_application()
