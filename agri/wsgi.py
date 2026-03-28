ef2b9b9cb385e724048db053403e39b59933e66e
itsMisbah <misbah77777@gmail.com>
2026-03-28 05:33:54 +0500

    create superuser

--- agri/wsgi.py
+++ agri/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
+from django.core.management import call_command #extra
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agri.settings')
 
@@ -22,4 +22,20 @@ try:
 except Exception as e:
     print(f"Migration error: {e}")
 
-application = get_wsgi_application()
+#superuser create
+try:
+    call_command('migrate', '--noinput')
+    
+    # Superuser create
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email=os.environ.get('ADMIN_EMAIL', 'admin@admin.com'),
+            password=os.environ.get('ADMIN_PASSWORD', 'admin123'),
+        )
+except Exception as e:
+    print(f"Setup error: {e}")
+
+
