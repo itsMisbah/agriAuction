d8cbfae8a80aa599948ae8937c63844e32226318
itsMisbah <misbah77777@gmail.com>
2026-05-10 12:52:32 +0500

    create superuser

--- build.sh
+++ build.sh
@@ -11,4 +11,6 @@ python manage.py collectstatic --no-input
 echo "Running migrations..."
 python manage.py migrate
 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(is_superuser=True).exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')" | python manage.py shell
+
 echo "Build completed successfully!"
