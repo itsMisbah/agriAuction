3de401fc5676f377976061ac897949101ccb5be8
itsMisbah <misbah77777@gmail.com>
2026-05-10 12:22:04 +0500

    update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,4 @@ python manage.py collectstatic --no-input
 echo "Running migrations..."
 python manage.py migrate
 
-echo "Creating superuser if none exists..."
-python manage.py create_superuser_if_none
-
 echo "Build completed successfully!"
